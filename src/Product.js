cd2b5555ceeffcfee4e58700fe2e0f4dc094bed0
wildenali <wildenwildenaliali@gmail.com>
2020-09-01 17:03:50 +0700

    bungkus info produk ke dalam div

--- src/Product.js
+++ src/Product.js
@@ -4,19 +4,21 @@ import './Product.css'
 function Product({ id, title, image, price, rating}) {
   return (
     <div className="product">
-      <p>{title}</p>
-      <p className="product__">
-        <small>USD</small>
-        <strong>{price}</strong>
-      </p>
-      <div className="product__rating">
-        {
-          Array(rating)
-          .fill()
-          .map((_) => (
-            <p>⭐</p>
-          ))
-        }
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__">
+          <small>USD</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {
+            Array(rating)
+            .fill()
+            .map((_) => (
+              <p>⭐</p>
+            ))
+          }
+        </div>
       </div>
       <img src={image} alt="" />
       <button>Add to basket</button>
