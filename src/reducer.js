ecc140b3b30261c1e7a03af5ba0800b842872aac
wildenali <wildenwildenaliali@gmail.com>
2020-09-02 01:39:05 +0700

    Membuat 2 data dummy untuk checkoutProduct

--- src/reducer.js
+++ src/reducer.js
@@ -1,11 +1,20 @@
 export const initialState = {
-  basket: [{
-    id: "12341234",
-    title: "The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses",
-    price: 11.96,
-    rating: 4,
-    image: "https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
-  }],
+  basket: [
+    {
+      id: "12341234",
+      title: "The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses",
+      price: 11.96,
+      rating: 4,
+      image: "https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
+    },
+    {
+      id: "12341234",
+      title: "The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses",
+      price: 11.96,
+      rating: 4,
+      image: "https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
+    },
+  ],
   user: null,
 }
 
