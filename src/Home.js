e61e11cd9f214eff1ca7da383a24b22b4b1fd7c2
wildenali <wildenwildenaliali@gmail.com>
2020-09-01 15:05:21 +0700

    Membuat halaman Home

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className="home">
+      <img
+        className="home__image"
+        src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Home_v2_en_US_1x._CB429090084_.jpg"
+        alt=""
+      />
+      <h1>I am Home Page 🏡</h1>
+    </div>
+  )
+}
+
+export default Home
