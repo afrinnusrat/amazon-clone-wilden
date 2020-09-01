89586660ca267fd5fe4c6443908e7389574ec1a2
wildenali <wildenwildenaliali@gmail.com>
2020-09-01 21:37:46 +0700

    Lupa import useContext

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -1,7 +1,7 @@
 // setup data layer
 // We need this to track the basket
 
-import React, { createContext, useState, useEffect, useReducer } from 'react';
+import React, { createContext, useState, useEffect, useReducer, useContext } from 'react';
 
 // THIS IS THE DATA LAYER
 export const StateContext = createContext();
