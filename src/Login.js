325fbc30b46b493bc7be4028940841d8474044e7
wildenali <wildenwildenaliali@gmail.com>
2020-09-02 12:56:09 +0700

    Membuat form Sign in, ada email dan password

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './Login.css'
+import { Link } from 'react-router-dom';
 
 function Login() {
   return (
-    <div>
-      
+    <div className="login">
+      <Link to="/">
+        <img
+          className="login__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1000px-Amazon_logo.svg.png"
+          alt=""
+        />
+      </Link>
+
+      <div>
+        <h1>Sign in</h1>
+        <form>
+          <h5>Email</h5>
+          <input type="text" />
+          <h5>Password</h5>
+          <input type="text" />
+        </form>
+      </div>
     </div>
   )
 }
