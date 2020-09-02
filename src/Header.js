0baaa9a7d95633f6b77fac8381fa32e2ae047083
wildenali <wildenwildenaliali@gmail.com>
2020-09-02 14:50:41 +0700

    bisa logout dan login

--- src/Header.js
+++ src/Header.js
@@ -4,12 +4,19 @@ import { Link } from 'react-router-dom'
 import SearchIcon  from '@material-ui/icons/Search';
 import ShoppingBasketIcon  from '@material-ui/icons/ShoppingBasket';
 import { useStateValue } from './StateProvider';
+import { auth } from './firebase';
 
 function Header() {
 
-  const [{ basket }] = useStateValue();   // cara akses ke react context
+  const [{ basket, user }] = useStateValue();   // cara akses ke react context
 
   console.log(basket);
+
+  const login = () => {
+    if (user) {
+      auth.signOut();
+    }
+  }
   
   return (
     <div className="header">
@@ -36,10 +43,10 @@ function Header() {
       {/* 3 Links */}
       <div className="header__nav">
         {/* 1st link */}
-        <Link to="/login" className="header__link">
-          <div className="header__option">
-            <span className="header__optionLineOne">Hai,</span>
-            <span className="header__optionLineTwo">Sign In</span>
+        <Link to={!user && "/login"} className="header__link">
+          <div onClick={login} className="header__option">
+            <span className="header__optionLineOne">Hai, {user?.email}</span>
+            <span className="header__optionLineTwo">{user ? 'Sign Out' : 'Sign In'}</span>
           </div>
         </Link>
 
