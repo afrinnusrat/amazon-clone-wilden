3de7a4461f6a695c449382ffe67bc3771ce3d7b9
wildenali <wildenwildenaliali@gmail.com>
2020-09-02 13:44:35 +0700

    membuat hooks untuk email dan password

--- src/Login.js
+++ src/Login.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css'
 import { Link } from 'react-router-dom';
+import { auth } from './firebase';
 
 function Login() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const login = event => {
     event.preventDefault();   // this stops the refresh
     // do the login logic...
-    
   }
   
   const register = event => {
@@ -30,9 +33,9 @@ function Login() {
         <h1>Sign in</h1>
         <form>
           <h5>Email</h5>
-          <input type="email" />
+          <input value={email} onChange={event => setEmail(event.target.value)} type="email" />
           <h5>Password</h5>
-          <input type="password" />
+          <input value={password} onChange={event => setPassword(event.target.value)} type="password" />
           <button onClick={login} type="submit" className="login_signInButton">Sign In</button>
         </form>
         <p>
